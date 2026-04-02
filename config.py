efee07f8e54b5c89e8ac54d52aa119a40a9b3afd
huryaq <hurryapurrya@gmail.com>
2026-04-02 20:21:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", "-1002609033575")) # C
 
 try:
     ADMINS = []
-    for x in (os.environ.get("ADMINS", "6521688979").split()):
+    for x in (os.environ.get("ADMINS", "8200246994 6521688979").split()):
         ADMINS.append(int(x))
 except ValueError:
     raise Exception("Your Admins list does not contain valid integers.")
